bc53d91ea5532953620a5f83a9d698a33b311164
Tobi3010 <tobiasandersen62@gmail.com>
2025-01-13 14:45:23 +0100

    fixed sound issues

--- Assets/Scenes/PortalScene/PortalSound.cs
+++ Assets/Scenes/PortalScene/PortalSound.cs
@@ -31,12 +31,13 @@ public class AudioTrackerNoListener : MonoBehaviour
     AudioSource GetDomAudio() {
         AudioSource domAudio = null;
         float maxVolume = 0f;
-
+        
         foreach (AudioSource audio in audios) {                        // Loop through all audios in our scene
-            if (audio == GetComponent<AudioSource>()) { continue; }    // Skip if its this portal's audio
+            if (audio == GetComponent<AudioSource>()) {
+                print("skipped!");
+                continue; }    // Skip if its this portal's audio
 
             if (audio.isPlaying) {                                     // Check if audio is actually playing
-
                 // Very basic attenuation formula, inverse square law (https://www.sciencedirect.com/topics/engineering/inverse-square-law)
                 float dist = Vector3.Distance(transform.position, audio.transform.position);
                 float volume = audio.volume / (dist * dist);          
